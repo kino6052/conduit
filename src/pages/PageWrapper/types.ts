a5550811c1fa379ff3600b75637eb26e73923de4
Kirill Novik <knovik@beeline.ru>
2024-03-16 21:19:10 +0300

    WIP

--- src/pages/PageWrapper/types.ts
+++
@@ -1,7 +0,0 @@
-import { TBannerProps } from "../../components/Banner/Banner";
-import { TPaginationBarProps } from "../../components/PaginationBar/PaginationBar";
-
-export type TPageWrapperProps = {
-  bannerProps: TBannerProps;
-  paginationBarProps?: TPaginationBarProps;
-};
