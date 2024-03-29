fdcb4933d971d7b098a890985a1c9d7932d03d6b
Kirill Novik <knovik@beeline.ru>
2024-03-29 10:39:09 +0300

    Refactor

--- src/logic/home/index.ts
+++ src/logic/home/index.ts
@@ -1,104 +1,34 @@
 import { filter, tap } from "rxjs";
 import { EArticleConstant } from "../../components/Article/constants";
 import { ETagConstant } from "../../components/Tag/constants";
-import { EPage, TAppProps } from "../../types";
+import { EPage } from "../../types";
 import {
   CurrentPageSubject,
-  IncomingEventSubject,
-  ResultingStateSubject,
+  IncomingEventSubject
 } from "../common.logic";
-import { ArticleDatabase } from "../data/article";
-import { UserDatabase } from "../data/user";
-import { provideNavbarProps } from "../utils/utils";
-import { AppState } from "../data/app";
+import { HomePage } from "./logic";
 
 IncomingEventSubject.pipe(
   filter((event) => event.slug === EArticleConstant.Slug),
-  tap((event) => {
-    const id = event.id;
-
-    if (!id) {
-      console.error("Item doesn't have an id");
-      return;
-    }
-
-    ArticleDatabase.setCurrentArticleId(id);
-    CurrentPageSubject.next(EPage.Article);
-  }),
+  tap(HomePage.handleArticleClick),
 ).subscribe();
 
 IncomingEventSubject.pipe(
   filter((event) => event.slug === EArticleConstant.LikeButtonSlug),
-  tap((event) => {
-    const id = event.id;
-
-    if (!id) return;
-
-    ArticleDatabase.likeArticleById(id);
-
-    CurrentPageSubject.next(EPage.Home);
-  }),
+  tap(HomePage.handleArticleLike),
 ).subscribe();
 
 IncomingEventSubject.pipe(
   filter((event) => event.slug === ETagConstant.Slug),
-  tap((event) => {
-    const id = event.id;
-
-    if (!id) return;
-
-    AppState.selectedTagId = id;
-
-    CurrentPageSubject.next(EPage.Home);
-  }),
+  tap(HomePage.selectTag),
 ).subscribe();
 
 IncomingEventSubject.pipe(
   filter((event) => event.slug === EArticleConstant.UserInfoSlug),
-  tap((event) => {
-    const id = event.id;
-
-    if (!id) return;
-
-    const article = ArticleDatabase.getArticleById(id);
-
-    const { userInfoProps } = article;
-
-    const userInfo = UserDatabase.findUserByName(userInfoProps.username);
-
-    AppState.selectedUserId = userInfo?.username;
-
-    CurrentPageSubject.next(EPage.Profile);
-  }),
+  tap(HomePage.handleUserInfoClick),
 ).subscribe();
 
 CurrentPageSubject.pipe(
   filter((page) => page === EPage.Home),
-  tap(() => {
-    const nextState: TAppProps<EPage.Home> = {
-      page: EPage.Home,
-      pageProps: {
-        posts: ArticleDatabase.getArticles().filter((post) => {
-          const selectedTag = ArticleDatabase.getAllTags().find(t => t.id === AppState.selectedTagId);
-
-          if (!selectedTag) return true;
-
-          const tag = post.tags.find(({ id }) => id === selectedTag.id);
-          return !!tag;
-        }),
-        paginationBarProps: {
-          numberOfPages: 1,
-          selected: 0,
-        },
-        sidebarProps: {
-          tags: ArticleDatabase.getAllTags(),
-          title: "Popular Tags",
-        },
-        tabs: [],
-      },
-      navbarProps: provideNavbarProps(),
-    };
-
-    ResultingStateSubject.next(nextState);
-  }),
+  tap(() => HomePage.update()),
 ).subscribe();
---
+++ src/logic/home/logic.ts
@@ -0,0 +1,87 @@
+import { EPage, TAppProps } from "../../types";
+import { IEvent } from "../../utils/events";
+import { CurrentPageSubject, ResultingStateSubject } from "../common.logic";
+import { AppState } from "../data/app";
+import { ArticleDatabase } from "../data/article";
+import { UserDatabase } from "../data/user";
+import { provideNavbarProps } from "../utils/utils";
+
+export class HomePage {
+  static handleArticleClick(event: IEvent) {
+    // TODO: Move into "article handlers"
+    const id = event.id;
+
+    if (!id) {
+      console.error("Item doesn't have an id");
+      return;
+    }
+
+    ArticleDatabase.setCurrentArticleId(id);
+    CurrentPageSubject.next(EPage.Article); // TODO: Change AppState instead
+  }
+
+  static handleArticleLike(event: IEvent) {
+    const id = event.id;
+
+    if (!id) return;
+
+    ArticleDatabase.likeArticleById(id);
+    CurrentPageSubject.next(EPage.Home);
+  }
+
+  static selectTag(event: IEvent) {
+    const id = event.id;
+
+    if (!id) return;
+
+    AppState.selectedTagId = id;
+
+    CurrentPageSubject.next(EPage.Home);
+  }
+
+  static handleUserInfoClick(event: IEvent) {
+    const id = event.id;
+
+    if (!id) return;
+
+    const article = ArticleDatabase.getArticleById(id);
+
+    const { userInfoProps } = article;
+
+    const userInfo = UserDatabase.findUserByName(userInfoProps.username);
+
+    AppState.selectedUserId = userInfo?.username;
+
+    CurrentPageSubject.next(EPage.Profile);
+  }
+
+  static update() {
+    const nextState: TAppProps<EPage.Home> = {
+      page: EPage.Home,
+      pageProps: {
+        posts: ArticleDatabase.getArticles().filter((post) => {
+          const selectedTag = ArticleDatabase.getAllTags().find(
+            (t) => t.id === AppState.selectedTagId,
+          );
+
+          if (!selectedTag) return true;
+
+          const tag = post.tags.find(({ id }) => id === selectedTag.id);
+          return !!tag;
+        }),
+        paginationBarProps: {
+          numberOfPages: 1,
+          selected: 0,
+        },
+        sidebarProps: {
+          tags: ArticleDatabase.getAllTags(),
+          title: "Popular Tags",
+        },
+        tabs: [],
+      },
+      navbarProps: provideNavbarProps(),
+    };
+
+    ResultingStateSubject.next(nextState);
+  }
+}
