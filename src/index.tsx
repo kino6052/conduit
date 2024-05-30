da9c2c2f7538e3692b43d9eb6ff089fe702e2b29
Kirill Novik <knovik@beeline.ru>
2024-05-30 22:27:26 +0300

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { defaultComposeApp } from "./details";
 import { render } from "./details/view";
 
+// This is the entry of the application, where we bootstrap a view model
 const viewModel = defaultComposeApp();
 
+// and then render it 
 render(viewModel);
